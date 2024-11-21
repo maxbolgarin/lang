34b0c2001128862544a57259b47bf9af8d1698ac
maxbolgarin <maxbolgarin@yandex.ru>
2024-11-21 13:15:23 +0300

    feat(recover): add new methods to recover panics

---
+++ recover.go
@@ -0,0 +1,72 @@
+package lang
+
+import (
+	"fmt"
+	"runtime/debug"
+)
+
+// Logger is an interface for logging panic stack trace and errors.
+type Logger interface {
+	Error(msg string, args ...any)
+}
+
+// Go runs goroutine with recover. It will print stack trace and restart goroutine in case of panic.
+// If you want to run goroutine without restarting after panic, just use go func() with Recover.
+func Go(l Logger, f func()) {
+	var foo func()
+	fn := f
+	foo = func() {
+		defer func() {
+			if err := recover(); err != nil {
+				printErrorWithStack(l, err)
+				go foo()
+			}
+		}()
+		fn()
+	}
+	go foo()
+}
+
+// Recover should be used with defer to recover and log stack trace in case of panic.
+func Recover(l Logger) bool {
+	if err := recover(); err != nil {
+		printErrorWithStack(l, err)
+		return true
+	}
+	return false
+}
+
+// RecoverWithErr should be used with defer to recover panic and return error from function without logging.
+func RecoverWithErr(outerError *error) bool {
+	if panicErr := recover(); panicErr != nil {
+		err := fmt.Errorf("%v", panicErr)
+		*outerError = err
+		return true
+	}
+	return false
+}
+
+// RecoverWithErrAndStack should be used with defer to recover panic and return error from function with logging stack.
+func RecoverWithErrAndStack(l Logger, outerError *error) bool {
+	if panicErr := recover(); panicErr != nil {
+		err := fmt.Errorf("%v", panicErr)
+		*outerError = err
+		printErrorWithStack(l, err)
+		return true
+	}
+	return false
+}
+
+// RecoverWithHandler should be used with defer to recover and call provided handler func.
+func RecoverWithHandler(handler func(err any)) bool {
+	if panicErr := recover(); panicErr != nil {
+		handler(panicErr)
+		return true
+	}
+	return false
+}
+
+func printErrorWithStack(l Logger, err any) {
+	stack := debug.Stack()
+	l.Error(string(stack), "error", err) // build with -trimpath to avoid printing build path in trace
+}
---
+++ recover_test.go
@@ -0,0 +1,132 @@
+package lang_test
+
+import (
+	"log/slog"
+	"strings"
+	"sync"
+	"sync/atomic"
+	"testing"
+
+	"github.com/maxbolgarin/lang"
+)
+
+type testLogger struct {
+	logs    atomic.Int64
+	lastMsg atomic.Value
+}
+
+func (s *testLogger) Error(msg string, args ...any) {
+	slog.Default().Error(msg, args...)
+	s.logs.Add(1)
+	s.lastMsg.Store(msg)
+}
+
+func TestGo(t *testing.T) {
+	var (
+		wg         = sync.WaitGroup{}
+		l          = testLogger{}
+		counter    atomic.Int64
+		logCounter = int64(5)
+	)
+
+	wg.Add(1)
+	lang.Go(&l, func() {
+		counter.Add(1)
+		if counter.Load() < logCounter {
+			panic("panic-error")
+		}
+		wg.Done()
+	})
+
+	wg.Wait()
+
+	if l.logs.Load() != logCounter-1 {
+		t.Errorf("expected %d logs", logCounter-1)
+	}
+}
+
+func TestRecover(t *testing.T) {
+	l := testLogger{}
+	defer func() {
+		if l.logs.Load() == 0 {
+			t.Error("expected at least one log")
+		}
+		if !strings.Contains(l.lastMsg.Load().(string), "lang_test.TestRecover") {
+			t.Error("expected stack trace in message")
+		}
+	}()
+	defer lang.Recover(&l)
+	panic("panic-error")
+}
+
+func TestRecoverWithErr(t *testing.T) {
+	var err error
+	defer func() {
+		if err == nil {
+			t.Error("expected error")
+		}
+		if !strings.Contains(err.Error(), "panic-error") {
+			t.Error("expected error in message")
+		}
+	}()
+	defer lang.RecoverWithErr(&err)
+	panic("panic-error")
+}
+
+func TestRecoverWithErrAndStack(t *testing.T) {
+	l := testLogger{}
+	var err error
+	defer func() {
+		if l.logs.Load() == 0 {
+			t.Error("expected at least one log")
+		}
+		if !strings.Contains(l.lastMsg.Load().(string), "lang_test.TestRecoverWithErrAndStack") {
+			t.Error("expected stack trace in message")
+		}
+		if err == nil {
+			t.Error("expected error")
+		}
+		if !strings.Contains(err.Error(), "panic-error") {
+			t.Error("expected error in message")
+		}
+	}()
+	defer lang.RecoverWithErrAndStack(&l, &err)
+	panic("panic-error")
+}
+
+func TestRecoverWithHandler(t *testing.T) {
+	var counter atomic.Int64
+	defer func() {
+		if counter.Load() == 0 {
+			t.Error("expected at least one log")
+		}
+	}()
+	defer lang.RecoverWithHandler(func(err any) {
+		counter.Add(1)
+	})
+	panic("panic-error")
+}
+
+func TestNoPanic(t *testing.T) {
+	l := testLogger{}
+	var err error
+	var counter atomic.Int64
+	defer func() {
+		if l.logs.Load() != 0 {
+			t.Error("not expected logs")
+		}
+		if err != nil {
+			t.Error("not expected error")
+		}
+		if counter.Load() != 0 {
+			t.Error("not expected counter")
+		}
+	}()
+
+	defer lang.Recover(&l)
+	defer lang.RecoverWithErr(&err)
+	defer lang.RecoverWithErrAndStack(&l, &err)
+	defer lang.RecoverWithHandler(func(err any) {
+		counter.Add(1)
+	})
+}
